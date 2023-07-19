5d85605dda6d2565552c605bc9894c0d2ee752a3
piperflo <69817139+piperflo@users.noreply.github.com>
2023-07-19 16:14:52 -0500

    Fixed Media

--- game-store/src/Components/Header.js
+++ game-store/src/Components/Header.js
@@ -24,7 +24,7 @@ const Header = ({cartItems, count}) =>{
                 <nav>
                     <ul className="main-list">
                         <li>
-                            <h1 className="Home" to="/Game-Store">GameStore</h1>
+                            <Link className="Home" to="/Game-Store">GameStore</Link>
                         </li>
                         <li>
                             <ul className="right-side">
